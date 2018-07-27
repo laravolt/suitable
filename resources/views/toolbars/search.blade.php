c9b2adcbd0cf44a4b34d85291295dd3ec695bf3b
FarisGenis <faaris513@gmail.com>
2018-07-27 10:50:56 +0700

    styling

--- resources/views/toolbars/search.blade.php
+++ resources/views/toolbars/search.blade.php
@@ -1,8 +1,8 @@
 <div class="item">
     <form method="GET" action="{{ request()->fullUrl() }}">
-        <div class="ui action input">
+        <div class="ui transparent action input">
             <input class="prompt" name="{{ $search }}" value="{{ request($search) }}" type="text" placeholder="@lang('suitable::suitable.search_placeholder')">
-            <button class="ui submit button mini icon"><i class="search link icon"></i></button>
+            <button class="ui submit basic button icon b-0"><i class="search link icon"></i></button>
         </div>
     </form>
 </div>
